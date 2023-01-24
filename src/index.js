eb586490db92539770b0388cd865445dbad9d2b2
Ali Shirani <alishiranii1384@gmail.com>
2023-01-24 20:17:59 +0330

    mobile menu completed

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import "./style.scss";
+
+const navBtn = document.querySelector(".menu-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
+const overlay = document.querySelector(".overlay");
+
+overlay.addEventListener("click", () => {
+    mobileMenu.classList.remove("toggle");
+    overlay.classList.remove("active");
+})
+navBtn.addEventListener("click", () => {
+    mobileMenu.classList.add("toggle");
+    overlay.classList.add("active");
+})
+
+
 feather.replace();
