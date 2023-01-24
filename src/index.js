8a6ccc54c2365cb0b245bad55d70f82e199b483a
Ali Shirani <alishiranii1384@gmail.com>
2023-01-24 20:19:01 +0330

    prettier

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const overlay = document.querySelector(".overlay");
 
 overlay.addEventListener("click", () => {
-    mobileMenu.classList.remove("toggle");
-    overlay.classList.remove("active");
-})
+  mobileMenu.classList.remove("toggle");
+  overlay.classList.remove("active");
+});
 navBtn.addEventListener("click", () => {
-    mobileMenu.classList.add("toggle");
-    overlay.classList.add("active");
-})
-
+  mobileMenu.classList.add("toggle");
+  overlay.classList.add("active");
+});
 
 feather.replace();
