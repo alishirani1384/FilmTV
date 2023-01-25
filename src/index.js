e0d2c177be155a0b6d71b2772d65090061e6c046
Ali Shirani <alishiranii1384@gmail.com>
2023-01-25 19:48:55 +0330

    sub menu mobile added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import "./style.scss";
 const navBtn = document.querySelector(".menu-btn");
 const mobileMenu = document.querySelector(".mobile-menu");
 const overlay = document.querySelector(".overlay");
+const mobileLinks = document.querySelectorAll(".mobile-link div");
+
+mobileLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    link.parentElement.classList.toggle("menu-toggle");
+  });
+});
 
 overlay.addEventListener("click", () => {
   mobileMenu.classList.remove("toggle");
